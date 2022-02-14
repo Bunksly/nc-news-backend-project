8eeb6be1322e8bef49f514ce58fc43cff0a71388
Bunksly <bunksly@gmail.com>
2022-02-14 12:16:03 +0000

    added /api/users path

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { getTopics } = require('./controllers/get-controllers')
+const { getTopics, getUsers } = require('./controllers/get-controllers')
 const { pathNotFoundErr, errorHandler } = require('./controllers/error-controllers')
 
 const app = express()
 
 app.get('/api/topics', getTopics)
+app.get('/api/users', getUsers)
 
 app.all('/*', pathNotFoundErr)
 
--- app/models/get-models.js
+++ app/models/get-models.js
@@ -2,7 +2,15 @@ const db = require('../../db/connection')
 
 exports.fetchTopics = () => {
     return db.query(`
-    SELECT * FROM topics
+    SELECT * FROM topics;
+    `).then(({ rows }) => {
+        return rows
+    })
+}
+
+exports.fetchUsers = () => {
+    return db.query(`
+    SELECT username FROM users;
     `).then(({ rows }) => {
         return rows
     })
