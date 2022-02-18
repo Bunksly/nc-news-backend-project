4b86e2447eb49983285dd15c3cb19d13ab0e0b88
Bunksly <bunksly@gmail.com>
2022-02-18 15:44:52 +0000

    hosting setup

--- app/listen.js
+++ app/listen.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
-app.listen(9090, () => {
-    console.log('listening...')
-})
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
