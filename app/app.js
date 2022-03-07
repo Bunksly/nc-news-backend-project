b8a1ca3f9e07e73da0daaa4b1f43fd96009c6f00
Bunksly <bunksly@gmail.com>
2022-03-07 10:46:35 +0000

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -1,32 +1,53 @@
-const express = require('express')
-const { getTopics, getUsers, getArticles, getArticleByID, patchArticleByID, getCommentsByArticleID, postComment, getUserByID, deleteCommentByID, getEndpoints, patchCommentByID, postTopic, postArticle, deleteArticleByID } = require('./controllers/get-controllers')
-const { pathNotFoundErr, errorHandler, handle500s } = require('./controllers/error-controllers')
+const express = require("express");
+const cors = require("cors");
+const {
+  getTopics,
+  getUsers,
+  getArticles,
+  getArticleByID,
+  patchArticleByID,
+  getCommentsByArticleID,
+  postComment,
+  getUserByID,
+  deleteCommentByID,
+  getEndpoints,
+  patchCommentByID,
+  postTopic,
+  postArticle,
+  deleteArticleByID,
+} = require("./controllers/get-controllers");
+const {
+  pathNotFoundErr,
+  errorHandler,
+  handle500s,
+} = require("./controllers/error-controllers");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/topics', getTopics)
-app.get('/api/users', getUsers)
-app.get('/api/articles', getArticles)
-app.get('/api/articles/:article_id', getArticleByID)
-app.get('/api/articles/:article_id/comments', getCommentsByArticleID)
-app.get('/api/users/:username', getUserByID)
-app.get('/api', getEndpoints)
+app.get("/api/topics", getTopics);
+app.get("/api/users", getUsers);
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id", getArticleByID);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleID);
+app.get("/api/users/:username", getUserByID);
+app.get("/api", getEndpoints);
 
-app.patch('/api/articles/:article_id', patchArticleByID)
-app.patch('/api/comments/:comment_id', patchCommentByID)
+app.patch("/api/articles/:article_id", patchArticleByID);
+app.patch("/api/comments/:comment_id", patchCommentByID);
 
-app.post('/api/articles/:article_id/comments', postComment)
-app.post('/api/topics', postTopic)
-app.post('/api/articles', postArticle)
+app.post("/api/articles/:article_id/comments", postComment);
+app.post("/api/topics", postTopic);
+app.post("/api/articles", postArticle);
 
-app.delete('/api/comments/:comment_id', deleteCommentByID)
-app.delete('/api/articles/:article_id', deleteArticleByID)
+app.delete("/api/comments/:comment_id", deleteCommentByID);
+app.delete("/api/articles/:article_id", deleteArticleByID);
 
-app.all('/*', pathNotFoundErr)
+app.all("/*", pathNotFoundErr);
 
-app.use(errorHandler)
-app.use(handle500s)
+app.use(errorHandler);
+app.use(handle500s);
 
-module.exports = app
+module.exports = app;
